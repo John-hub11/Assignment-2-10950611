6298d437bb2ea86a84a86aca7825ce09a2f222d6
Johnson Franklin Selasi <fsjohnson001@st.ug.edu.gh>
2024-07-06 12:17:25 -0700

    Completed Interfaces App

---
+++ Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Interface IMovable with a method Move()
+public interface IMovable
+{
+    void Move();
+}
+
+// Class Car that implements IMovable
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class Bicycle that implements IMovable
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of Car and Bicycle
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        // Call the Move() method on each instance
+        car.Move();
+        bicycle.Move();
+    }
+}
